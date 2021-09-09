01e358f4684cff1776f816e1eac7de477cffcf0a
Jozh <jozhgonzales@csus.edu>
2021-09-08 18:50:26 -0700

    Organized stuff. Setup initial React app.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Spring2021_Codevid
-
+# Codevid
---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+class App extends Component
+{
+    render(): JSX.Element
+    {
+        return (
+            <div>
+                TOFO: Everything
+            </div>
+        );
+    }
+}
+
+export { App };
