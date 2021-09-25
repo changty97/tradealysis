29c27f49164c104c8c1d3c71ba219a57d2de8460
Jonathan Huerta <jhuerta@DESKTOP-Q5E45LN.localdomain>
2021-09-24 21:28:35 -0700

    Add images for home.html in react

--- frontend/src/components/HomeComponent.tsx
+++ frontend/src/components/HomeComponent.tsx
@@ -4,6 +4,10 @@ import { IReportsState } from "../models/IReportsState";
 // import { Link } from "react-router-dom";
 import { NavBarComponent } from "./NavBarComponent";
 
+import dataIcon from "../images/dataIcon.jpg";
+
+//import * from "./styleSheets/headerNavBar.css";
+
 class HomeComponent extends Component<IReportsProps, IReportsState>
 {
     constructor(props: IReportsProps)
@@ -20,8 +24,106 @@ class HomeComponent extends Component<IReportsProps, IReportsState>
     {
         return (
             <Fragment>
-                <NavBarComponent/>
-                You are on the home page.
+                <html>
+                    <body>
+                        <header id="allHeader">
+                            <NavBarComponent/>
+                            {/*
+                            <div className="theHeader">
+                                <div>
+                                    <h1>
+                                        <a href="./home.html">
+                                            <img src="./logo.jpg" alt="Tradalysis Logo" width="85%" />
+                                        </a>
+                                    </h1>
+                                </div>
+                                <div className="headerButtons">
+                                    <ul className="headerButtonsList">
+                                        <li>
+                                            <a href="./home.html">
+													Home
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="./report.html">
+													Report
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="userAndSettingButtons">
+                                    <ul className="headerButtonsList">
+                                        <li>
+                                            <a href="./account.html">
+													User
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="./account1.html">
+													Settings
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+						**/ }
+                        </header>
+							Test123
+                        <section>
+                            <div className="leftHome">
+                                <div className="leftHomeMainListDiv noWrap">
+                                    <ul className="leftHomeMainList pageOn">
+                                        <li>
+												Home
+                                        </li>
+                                    </ul>
+                                    <ul className="leftHomeMainList">
+                                        <button>
+											Import Broker Files
+                                        </button>
+                                    </ul>
+                                    
+									
+									
+									
+                                </div>
+                            </div>
+                            <div className="rightHome">
+                                <div className="dataIconDiv">
+                                    <img src={dataIcon} className="dataIcon" />
+										
+                                </div>
+                            </div>
+                        </section>
+                        <footer>
+                            <div>
+                                <ul>
+                                    <li>
+                                        <a href="./account1.html">
+												Your Account
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="./support.html">
+												Support
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="./about.html">
+												About
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="./privacy.html">
+												Privacy Policy
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </footer>
+		
+                    </body>
+                </html>
             </Fragment>
         );
     }
--- frontend/src/components/NavBarComponent.tsx
+++ frontend/src/components/NavBarComponent.tsx
@@ -1,9 +1,9 @@
 import { Component, Fragment } from "react";
 import { IReportsProps } from "../models/IReportsProps";
 import { IReportsState } from "../models/IReportsState";
-import companyLogo from "../images/logo.jpg";
 
-import { Link } from "react-router-dom";
+/* import companyLogo from "../images/logo.jpg"; */
+//import { Link } from "react-router-dom";
 
 class NavBarComponent extends Component<IReportsProps, IReportsState>
 {
@@ -21,21 +21,59 @@ class NavBarComponent extends Component<IReportsProps, IReportsState>
     {
         return (
             <Fragment>
+			
+                {/*
                 <div>
-                    <button>
-                        <img src={companyLogo} alt="Tradealysis Logo" width="50" height="50"/>
-                    </button>
+                    <button><img src={companyLogo} alt="Tradealysis Logo" width="50" height="50"/> </button>
                 </div>
-                <Link to="/home">
-                    <button>Home</button>
-                </Link>
-                <Link to="/report">
-                    <button>Report</button>
-                </Link>
+                <Link to="/home"><button>Home</button></Link>
+                <Link to="/report"><button>Report</button></Link>
                 <button>User</button>
                 <button>Settings</button>
-                <p>This is the end of the NavBar Component</p>
-                <hr></hr>
+				
+                <p>
+					This is the end of the NavBar Component
+                </p>
+                <hr>
+                </hr>
+			*/}
+                <div className="theHeader">
+                    <div>
+                        <h1>
+                            <a href="./home.html">
+                                <img src="./logo.jpg" alt="Tradalysis Logo" width="85%" />
+                            </a>
+                        </h1>
+                    </div>
+                    <div className="headerButtons">
+                        <ul className="headerButtonsList">
+                            <li>
+                                <a href="/home">
+									Home
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/report">
+									Report
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="userAndSettingButtons">
+                        <ul className="headerButtonsList">
+                            <li>
+                                <a href="">
+									User
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+									Settings
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </Fragment>
         );
     }
