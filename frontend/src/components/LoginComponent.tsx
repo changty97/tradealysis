ab4e13a2fb5410ebaaccb436f6cc5134885523ea
Jonathan Huerta <jhuerta@DESKTOP-Q5E45LN.localdomain>
2021-10-08 21:44:09 -0700

    Front End Changes only

--- frontend/src/components/LoginComponent.tsx
+++ frontend/src/components/LoginComponent.tsx
@@ -1,36 +1,95 @@
 import { Component, Fragment } from "react";
-import { All } from "../cssComponents/All";
-import { NavBarLoginComponent } from "./NavBarLoginComponent";
 import { Login } from "../cssComponents/Login";
-import { FooterLoginComponent } from "./FooterLoginComponent";
 
-class LoginComponent extends Component
+import axios from "axios";
+const api = axios.create({
+    baseURL: 'http://localhost:3001/'
+});
+
+
+class LoginComponent extends Component<any, any>
 {
-    render(): JSX.Element
-    {
-        return (
-            <Fragment>
-                <All.HTML><style>{'body { background-color: rgb(170,170,170); }'}</style>
-                    <All.HTML_BODY>
-                        <NavBarLoginComponent />
-                        <Login.SECTION>
-                            <Login.LOGIN_BOX>
-                                <Login.USERNAME_AND_PASSWORD_TXT_BOXES>
-                                    <Login.USERNAME_OR_PSWD_INPUT type="text" placeholder="Username" />
-                                    <Login.USERNAME_OR_PSWD_INPUT type="text" placeholder="Password" />
-                                </Login.USERNAME_AND_PASSWORD_TXT_BOXES>
-                                <Login.LOGIN_BUTTON>
-                                    <Login.FORGOT_PSSD_BUTTON>Login</Login.FORGOT_PSSD_BUTTON>
-                                    <Login.FORGOT_PSSD_BUTTON>Forgot Password?</Login.FORGOT_PSSD_BUTTON>
-                                    <Login.FORGOT_PSSD_BUTTON>Sign Up</Login.FORGOT_PSSD_BUTTON>
-                                </Login.LOGIN_BUTTON>
-                            </Login.LOGIN_BOX>
-                        </Login.SECTION>
-                        <FooterLoginComponent/>
-                    </All.HTML_BODY>
-                </All.HTML>
-            </Fragment>
-        );
-    }
+	public loginState : boolean;
+	
+	constructor(props: any)
+	{
+	    super(props);
+	    this.loginState = false;
+	    this.login = this.login.bind(this);
+	}
+	
+	public getLoginState():boolean
+	{
+	    return this.loginState;
+	}
+
+	private login(): void
+	{
+	    if (document != null)
+	    {
+	        const str1 = document.getElementById(`uname`);
+	        const str2 = document.getElementById(`pssd`);
+	        if (str1 != null && str2 != null)
+	        {
+	            const str1Value = (str1 as HTMLInputElement).value;
+	            const str2Value = (str2 as HTMLInputElement).value;
+	            api.get('testGetDb', {
+	                params: {
+	                    username: `${str1Value}`,
+	                    password: `${str2Value}`
+	                }
+	            }).then(res =>
+	            {
+	                const myNum = Number(res.data);
+	                alert(myNum);
+
+	                if (myNum === 1)
+	                {
+	                    this.loginState = true;
+	                    alert("Login");
+	                }
+	                else
+	                {
+	                    alert("Not Login");
+	                }
+	            });
+	        }
+	    }
+	}
+	
+	render(): JSX.Element
+	{
+	    return (
+	        <Fragment>
+	            
+	            <Login.SECTION>
+	                <Login.LOGIN_BOX>
+	                    <Login.LOGIN_LABEL_DIV>
+	                        <label>Login</label>
+	                    </Login.LOGIN_LABEL_DIV>
+	                        <Login.USERNAME_AND_PASSWORD_TXT_BOXES>
+	                            <Login.USERNAME_AND_PASSWORD_TXT_BOXE_LABELS>Username</Login.USERNAME_AND_PASSWORD_TXT_BOXE_LABELS>
+	                            <Login.USERNAME_OR_PSWD_INPUT
+	                                id="uname"
+	                                type="text"
+	                                placeholder=""
+	                            />
+	                            <Login.USERNAME_AND_PASSWORD_TXT_BOXE_LABELS>Password</Login.USERNAME_AND_PASSWORD_TXT_BOXE_LABELS>
+	                            <Login.USERNAME_OR_PSWD_INPUT
+	                                id="pssd"
+	                                type="password"
+	                                placeholder=""
+	                            />
+	                        </Login.USERNAME_AND_PASSWORD_TXT_BOXES>
+						   <Login.LOGIN_BUTTON>
+	                        <Login.FORGOT_PSSD_BUTTON type="submit" onClick={this.login}>Login</Login.FORGOT_PSSD_BUTTON>
+	                            <Login.FORGOT_PSSD_BUTTON>Forgot Password ?</Login.FORGOT_PSSD_BUTTON>
+	                            <Login.FORGOT_PSSD_BUTTON>Sign Up</Login.FORGOT_PSSD_BUTTON>
+	                        </Login.LOGIN_BUTTON>
+	                </Login.LOGIN_BOX>
+	            </Login.SECTION>
+	        </Fragment>
+	    );
+	}
 }
 export { LoginComponent };
--- frontend/src/components/PrivacyPolicyComponent.tsx
+++ frontend/src/components/PrivacyPolicyComponent.tsx
@@ -1,8 +1,5 @@
 import { Component, Fragment } from "react";
-import { All } from "../cssComponents/All";
-import { NavBarComponent } from "./NavBarComponent";
 import { Privacy } from "../cssComponents/Privacy";
-import { FooterComponent } from "./FooterComponent";
 
 class PrivacyPolicyComponent extends Component
 {
@@ -10,15 +7,9 @@ class PrivacyPolicyComponent extends Component
     {
         return (
             <Fragment>
-                <All.HTML><style>{'body { background-color: rgb(170,170,170); }'}</style>
-                    <All.HTML_BODY>
-                        <NavBarComponent/>
-                        <Privacy.SECTION>
-                            <Privacy.LEFTHOME><h1>Privacy Policy</h1></Privacy.LEFTHOME>
-                        </Privacy.SECTION>
-                        <FooterComponent/>
-                    </All.HTML_BODY>
-                </All.HTML>
+                <Privacy.SECTION>
+                    <Privacy.LEFTHOME><h1>Privacy Policy</h1></Privacy.LEFTHOME>
+                </Privacy.SECTION>
             </Fragment>
         );
     }
