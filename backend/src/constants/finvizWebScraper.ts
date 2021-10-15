81248143a78b6326a73750d4497686db706af546
Kevin Vong <kevinvong@Kevin-L.local>
2021-10-15 13:39:42 -0700

    Added function to scrape finviz data in backend. function is not currently used anywhere

---
+++ backend/src/constants/finvizWebScraper.ts
@@ -0,0 +1,60 @@
+
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+const finvizURL = 'https://finviz.com/quote.ashx?t=arec';
+
+function getFinvizData(): string[]
+{
+    const stockInfo: string[] = [];
+    axios(finvizURL)
+        .then(response =>
+        {
+            const html = response.data as string;
+            const $ = cheerio.load(html);
+            // Price
+            $('.table-dark-row:nth-child(11) .snapshot-td2:nth-child(12)', html).each(function()
+            {
+                const price = $(this).text();
+                stockInfo.push(price);
+            });
+            // 52-Week High; 52-Week Low
+            $('.table-dark-row:nth-child(6) .snapshot-td2:nth-child(10)', html).each(function()
+            {
+                const high = $(this).text().substr(0, $(this).text().indexOf(' '));
+                stockInfo.push(high);
+                const low = $(this).text().substr($(this).text().indexOf(' ') + 3);
+                stockInfo.push(low);
+
+            });
+            // Volume Average
+            $('.table-dark-row:nth-child(11) .snapshot-td2:nth-child(10)', html).each(function()
+            {
+                const average = $(this).text();
+                stockInfo.push(average);
+            });
+            // Sector and Industry Name
+            $('.fullview-title .fullview-links .tab-link:nth-child(2)', html).each(function()
+            {
+                const industry = $(this).text();
+                stockInfo.push(industry);
+            });
+            // Shares Outstanding
+            $('.table-dark-row:nth-child(1) .snapshot-td2:nth-child(10)', html).each(function()
+            {
+                const outstanding = $(this).text();
+                stockInfo.push(outstanding);
+            });
+            // Shares Float
+            $('.table-dark-row:nth-child(2) .snapshot-td2:nth-child(10)', html).each(function()
+            {
+                const float = $(this).text();
+                stockInfo.push(float);
+            });
+            // console.log("Finviz Data: ", stockInfo);
+        }).catch(err => console.log(err));
+    console.log("Finviz Data: ", stockInfo);
+    return stockInfo;
+}
+
+export { getFinvizData };
