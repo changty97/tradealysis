ee1515d4b94fed7a7280eb428b62114282b18525
Jozh <jozhgonzales@csus.edu>
2021-11-04 14:34:40 -0700

    Added an endpoint that lists all sessions in a collection.

--- backend/src/controllers/ServiceController.ts
+++ backend/src/controllers/ServiceController.ts
@@ -2,12 +2,13 @@ import { FileParam, FormParam, GET, POST,  Path, QueryParam, PathParam } from "t
 import { Produces, Response } from "typescript-rest-swagger";
 import { BadRequestError } from "typescript-rest/dist/server/model/errors";
 import { CSVParser } from "../CSVParser";
-import { saveTable } from "../MongoFiles/Mongo";
+import { getAllSessions, saveTable } from "../MongoFiles/Mongo";
 import { correctLoginKey, userFromKey } from "../MongoFiles/MongoLogin";
 import { createAccount } from "../MongoFiles/MongoCreateAccount";
 import { getStockData } from "../stockapi";
 import { ITableData } from "../models/ITableData";
 import { accountValueFromKey } from "../MongoFiles/MongoAccountSettings";
+import { ISession } from "../models/ISession";
 
 const badRequestExampleResponse: BadRequestError = {
     name: "BadRequestError",
@@ -134,5 +135,12 @@ export class ServiceController
 	public async accountbdateFromKeyGET(@QueryParam("key") key: string):Promise<string>
 	{
 	    return await accountValueFromKey(key, "bdate");
+	}
+
+	@Path("/getSessionList")
+	@GET
+	public async getSessionList(): Promise<ISession[]>
+	{
+	    return await getAllSessions();
 	}
 }
