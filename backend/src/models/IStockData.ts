a69661bfc7e22e0dae213c328400cea51659422a
Jozh <jozhgonzales@csus.edu>
2021-11-09 14:42:52 -0800

    Changed stock api endpoint and manually merged Tyler's code.

---
+++ backend/src/models/IStockData.ts
@@ -0,0 +1,23 @@
+export interface IStockData {
+    LongName?: string;
+    Ticker?: string;
+    Industry?: string;
+    Exchange?: string;
+    Price?: number;
+    W52H?: number;
+    W52L?: number;
+    VolAvg?: number;
+    Outstanding?: number
+    Float?: number;
+    VolDOI?: number;
+    VolPreM?: number;
+    PC?: number;
+    PremHigh?: number;
+    Open?: number;
+    HOD?: number;
+    HODTime?: string;
+    LOD?: number;
+    LODTime?: string;
+    Close?: number;
+    AH?: number;
+}
