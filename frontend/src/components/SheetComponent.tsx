fb74732e29879eb033a2619319c33917c96919e5
Kevin Vong <kevinvong@Kevin-L.local>
2021-11-11 11:16:43 -0800

    aafetch now populates all columns. table savves after each fetch

--- frontend/src/components/SheetComponent.tsx
+++ frontend/src/components/SheetComponent.tsx
@@ -109,8 +109,6 @@ class SheetComponent extends Component<ISheetComponentProps, ISheetComponentStat
     private saveTable(): void
     {
         const tableData = this.state.tableProps.data;
-        console.log(tableData);
-        console.log(tableData!.length);
         axios.post(`http://localhost:3001/postTableDB`, {
             data: {
                 table: tableData,
@@ -122,6 +120,198 @@ class SheetComponent extends Component<ISheetComponentProps, ISheetComponentStat
         }).catch(function(error)
         {
             console.log('Error', error);
+        });
+    }
+
+    getTicker(cell: any): void
+    {
+        if (this.state.tableProps.data)
+        {
+            // i is each table row
+            const i = this.state.tableProps.data[cell.rowKeyValue];
+            for (const [key, value] of Object.entries(i))
+            {
+                // return past data using Ticker and DOI value in current row
+                if (`${key}` === 'Ticker' && `${value}` !== '' && this.isValidDate(i.DOI))
+                {
+                    console.log(`getting past data for ${i.Ticker} on ${i.DOI}`);
+                    const pastData = this.getPastData(i.Ticker, i.DOI);
+                    console.log(pastData);
+                    this.setCells(pastData, cell);
+                }
+                // return today's data
+                else if (`${key}` === 'Ticker' && `${value}` !== '')
+                {
+                    console.log("Getting today's data");
+                    const todayData = this.getTodayData(i.Ticker);
+                    console.log(todayData);
+                    this.setCells(todayData, cell);
+                }
+            }
+        }
+        // save table each time new data is fetched
+        this.saveTable();
+    }
+
+    getTodayData(ticker: string): any
+    {
+        return axios.get(`http://localhost:3001/stockapi/${ticker}`, {
+            params: {
+                ID: ticker
+            }
+        }).then((response) =>
+        {
+            console.log("getTodayData returned: ");
+            console.log(response.data);
+            return response.data;
+        }).catch(function(error)
+        {
+            console.log('Error', error);
+        });
+    }
+    
+    getPastData(ticker: string, date: string): any
+    {
+        return axios.get(`http://localhost:3001/stockapi/${ticker}/${date}`, {
+            params: {
+                ID: ticker
+            }
+        }).then((response) =>
+        {
+            console.log("getPastData returned: ");
+            console.log(response.data);
+            return response.data;
+        }).catch(function(error)
+        {
+            console.log('Error', error);
+        });
+    }
+
+    // check YYYY-MM-DD format
+    isValidDate(doi: any): boolean
+    {
+        const regEx = /^\d{4}-\d{2}-\d{2}$/;
+        if (!doi.match(regEx))
+        {
+            return false;
+        }  // Invalid format
+        const d = new Date(doi);
+        const dNum = d.getTime();
+        if (!dNum && dNum !== 0)
+        {
+            return false;
+        } // NaN value, Invalid date
+        return d.toISOString().slice(0, 10) === doi;
+    }
+
+    setCells(data: any, cell: any): void
+    {
+        Promise.resolve(data).then((value) =>
+        {
+            if (this.state.tableProps.data)
+            {
+                // check each row
+                for (const [k, v] of Object.entries(this.state.tableProps.data[cell.rowKeyValue]))
+                {
+                    const i = this.state.tableProps.data[cell.rowKeyValue];
+                    if (`${k}` === 'Ticker' && `${v}` !== '')
+                    {
+                        console.log(value);
+                        // check each alpha vantage object property
+                        for (const [key, val] of Object.entries(value))
+                        {
+                            switch (key)
+                            {
+                            case "Price": i["Price"] = val;
+                                break;
+                            case "W52H": i["52-WH"] = val;
+                                break;
+                            case "W52L": i["52-WL"] = val;
+                                break;
+                            case "VolAvg": i["VolAvg"] = val;
+                                break;
+                            case "Outstanding": i.Outstanding = val;
+                                break;
+                            case "Float": i.Float = val;
+                                break;
+                            case "Industry": i.Industry = val;
+                                break;
+                            case "PC": i.PC = val;
+                                break;
+                            case "PremHigh": i["PreM High"] = val;
+                                break;
+                            case "Open": i["Open"] = val;
+                                break;
+                            case "HOD": i["HOD"] = val;
+                                break;
+                            case "HODTime": i["HOD-Time"] = val;
+                                break;
+                            case "LOD": i["LOD"] = val;
+                                break;
+                            case "LODTime": i["LOD-Time"] = val;
+                                break;
+                            case "Close": i["Close"] = val;
+                                break;
+                            case "AH": i["AH"] = val;
+                                break;
+                            case "VolDOI": i["Vol-DOI"] = val;
+                                break;
+                            case "VolPreM": i["Vol-PreM"] = val;
+                                break;
+                            default:
+                            }
+                        }
+                    }
+                    // populate calculated data
+                    // Float Rotation = Volume-DOI / Float
+                    i["FloatR"] = parseFloat((i["Vol-DOI"] / i["Float"]).toFixed(4));
+                    // Float Category is defined by size of float
+                    console.log(i.Float);
+                    if ( i.Float > 0 && i.Float <= 1000000)
+                    {
+                        i["FloatC"] = "NANO";
+                    }
+                    else if ( i.Float > 1000000 && i.Float <= 2000000)
+                    {
+                        i["FloatC"] = "MICRO";
+                    }
+                    else if ( i.Float > 2000000 && i.Float <= 10000000)
+                    {
+                        i["FloatC"] = "LOW";
+                    }
+                    else if ( i.Float > 10000000 && i.Float <= 20000000)
+                    {
+                        i["FloatC"] = "MEDIUM";
+                    }
+                    else if ( i.Float > 20000000)
+                    {
+                        i["FloatC"] = "HIGH";
+                    }
+                    // MarketCap = price * outstanding
+                    i["MC-Current"] = i["Price"] * i["Outstanding"];
+                    // MarketCap Category
+                    if ( i["MC-Current"] <= 50000000)
+                    {
+                        i["MC-Cat"] = "NANO";
+                    }
+                    else if ( i["MC-Current"] > 50000000 && i["MC-Current"] <= 300000000)
+                    {
+                        i["MC-Cat"] = "MICRO";
+                    }
+                    else if ( i["MC-Current"] > 300000000 && i["MC-Current"] <= 20000000000)
+                    {
+                        i["MC-Cat"] = "SMALL";
+                    }
+                    else if ( i["MC-Current"] > 20000000000 && i["MC-Current"] <= 100000000000)
+                    {
+                        i["MC-Cat"] = "MID";
+                    }
+                    else if ( i["MC-Current"] > 100000000000)
+                    {
+                        i["MC-Cat"] = "LARGE";
+                    }
+                }
+            }
         });
     }
 
@@ -147,16 +337,17 @@ class SheetComponent extends Component<ISheetComponentProps, ISheetComponentStat
                 {
                     return undefined;
                 }
+      
                 const cell = {
                     columnKey: column.key,
                     rowKeyValue
                 };
-                const isFocused = cell.columnKey === tableProps.focused?.cell?.columnKey &&
-              cell.rowKeyValue === tableProps.focused?.cell?.rowKeyValue;
+                const isFocused = cell.columnKey === this.state.tableProps.focused?.cell?.columnKey &&
+              cell.rowKeyValue === this.state.tableProps.focused?.cell?.rowKeyValue;
                 return {
                     tabIndex: 0,
                     ref: (ref: any) => isFocused && ref?.focus(),
-                    onKeyUp: (e) =>
+                    onKeyDown: (e) =>
                     {
                         switch (e.key)
                         {
@@ -169,17 +360,23 @@ class SheetComponent extends Component<ISheetComponentProps, ISheetComponentStat
                         case "ArrowUp": this.dispatch(moveFocusedUp({
                             end: e.ctrlKey
                         })); break;
-                        case "ArrowDown": this.dispatch(moveFocusedDown({
-                            end: e.ctrlKey
-                        })); break;
+                        case "ArrowDown":
+                            console.log("arrow down");
+                            this.dispatch(moveFocusedDown({
+                                end: e.ctrlKey
+                            })); break;
                             // opens the editor for the selected cell
                         case "Enter":
                             this.dispatch(openEditor(cell.rowKeyValue, cell.columnKey));
+                            
                             this.dispatch(setFocused({
                                 cellEditorInput: cell
                             }));
+                            
+                            console.log("enter key pressed");
                             break;
                         }
+                        
                     },
                     onFocus: () => !isFocused &&  this.dispatch(setFocused({
                         cell: {
@@ -187,7 +384,7 @@ class SheetComponent extends Component<ISheetComponentProps, ISheetComponentStat
                             rowKeyValue
                         }
                     })),
-                    onKeyDown: (e) => e.key !== "Tab" && e.preventDefault(),
+                    //onKeyUp: (e) => e.key !== "Tab" && this.dispatch(closeEditor(cell.rowKeyValue, cell.columnKey)) && console.log("closed"),
                     onBlur: () => isFocused && this.dispatch(clearFocused())
                 };
             },
@@ -197,21 +394,29 @@ class SheetComponent extends Component<ISheetComponentProps, ISheetComponentStat
                 column, rowKeyValue
             }) =>
             {
-                const isFocused = column.key === tableProps.focused?.cellEditorInput?.columnKey &&
-                rowKeyValue === tableProps.focused?.cellEditorInput?.rowKeyValue;
+                const isFocused = column.key === this.state.tableProps.focused?.cellEditorInput?.columnKey &&
+                rowKeyValue === this.state.tableProps.focused?.cellEditorInput?.rowKeyValue;
                 const cell = {
                     columnKey: column.key,
                     rowKeyValue
                 };
                 return {
                     ref: (ref: any) => isFocused && ref?.focus(),
-                    onKeyUp: (e) => e.key === "Enter" && this.dispatch(setFocused({
-                        cell
-                    })),
+                    onKeyUp: (e) =>
+                    {
+                        if (e.key === "Enter")
+                        {
+                            this.dispatch(setFocused({
+                                cell
+                            }));
+                            console.log("Enter key released");
+                        }
+                    },
                     onBlur: (e, {
                         baseFunc
                     }) =>
                     {
+                        console.log("Blur");
                         baseFunc();
                         this.dispatch(clearFocused());
                     },
@@ -315,6 +520,25 @@ class SheetComponent extends Component<ISheetComponentProps, ISheetComponentStat
                 tableProps: kaReducer(prevState.tableProps, action)
             }
         }));
+
+        if (action.columnKey === "Ticker" && action.type === "UpdateCellValue")
+        {
+            const cell = {
+                columnKey: action.columnKey,
+                rowKeyValue: action.rowKeyValue
+            };
+            console.log("Cell: ");
+            console.log(cell);
+            this.getTicker(cell);
+        }
+        if (action.columnKey === "DOI" && action.type === "UpdateCellValue")
+        {
+            const cell = {
+                columnKey: action.columnKey,
+                rowKeyValue: action.rowKeyValue
+            };
+            this.getTicker(cell);
+        }
     }
 }
 export { SheetComponent };
