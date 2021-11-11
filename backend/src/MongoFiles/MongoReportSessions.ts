95524d58b8b3a7c18d3139d2437ae00e0f3d2854
Jonathan Huerta <jhuerta@DESKTOP-Q5E45LN.localdomain>
2021-11-11 11:29:55 -0800

    back end code cleanup

---
+++ backend/src/MongoFiles/MongoReportSessions.ts
@@ -0,0 +1,46 @@
+import { MongoClient, Db, Collection } from "mongodb";
+import { userMongoOptions } from "../constants/globals";
+
+async function allUserSessions(key: string):Promise<string[]>
+{
+    let client: MongoClient | null = null;
+    return MongoClient.connect(userMongoOptions.uri).then((connection: MongoClient) =>
+    {
+        client = connection;
+        const db: Db = client.db(userMongoOptions.db);
+        const theCollectionKeyTable: Collection = db.collection(userMongoOptions.collections['userKey']);
+        const theCollectionSessionsTable: Collection = db.collection(userMongoOptions.collections['userSessions']);
+
+        return theCollectionKeyTable.distinct("user_obj_id", {
+            "key": key
+        }).then((results) =>
+        {
+            const emptyArr:string[] = [];
+            if (results && results.length !== 0)
+            {
+                return theCollectionSessionsTable.distinct(
+                    "session_ids", {
+                        "user_obj_id": results[0]
+                    }
+                ).then((results2: string[] ) =>
+                {
+                    return (results2) ? results2 : emptyArr;
+                });
+            }
+            return emptyArr;
+        });
+    })
+        .catch((err: Error) =>
+        {
+            return Promise.reject(err);
+        })
+        .finally(() =>
+        {
+            if (client)
+            {
+                client.close();
+            }
+        });
+}
+
+export { allUserSessions };
