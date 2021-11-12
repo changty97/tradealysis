521b0c2280f733ad24851b5a674b542ed4e3fbfd
Jozh <jozhgonzales@csus.edu>
2021-11-11 19:34:06 -0800

    Renamed Overview to OverviewComponent for consistency.

---
+++ frontend/src/components/OverviewComponent.tsx
@@ -0,0 +1,20 @@
+import { Component, Fragment } from "react";
+import { Link } from "react-router-dom";
+import { Reports } from "../cssComponents/Reports";
+
+class OverviewComponent extends Component
+{
+    render(): JSX.Element
+    {
+        return (
+            <Fragment>
+                <Reports.SECTION>
+                    <Link to="/report"><button>Trade Report</button></Link>
+                    <Link to="/overview"><button>Overview</button></Link>
+                    <Link to="/strategies"><button>Strategies</button></Link>
+                </Reports.SECTION>
+            </Fragment>
+        );
+    }
+}
+export { OverviewComponent };
