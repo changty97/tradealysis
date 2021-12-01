9dcc88c789eb6fc9eb1176672111d8afa4aa09e7
Alex Leung <mazato1996@yahoo.com>
2021-12-01 11:27:27 -0800

    Support page, with email contact

--- frontend/src/components/SupportComponent.tsx
+++ frontend/src/components/SupportComponent.tsx
@@ -1,34 +1,123 @@
 import { Component, Fragment } from "react";
-import { Support } from "../cssComponents/Support";
+//import { Support } from "../cssComponents/Support";
+import * as emailjs from 'emailjs-com';
+import { init } from 'emailjs-com';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+//import { ISupportProps } from "../models/ISupportProps";
+//import { ISupportState } from "../models/ISupportState";
+init("user_EXb9DCu4js07untJgFSES");
 
 class SupportComponent extends Component
 {
+    state = {
+        name: '',
+        email: '',
+        subject: '',
+        message: '',
+    }
+    //this.handleChange = this.handleChange.bind(this);
+    //this.handleSubmit = this.handleSubmit.bind(this);
+
+    handleSubmit(e: any)
+    {
+        e.preventDefault();
+
+        const {
+            email, subject, message
+        } = this.state;
+
+        const templateParams = {
+            from_name: email,
+            to_name: 'Tradealysis',
+            subject: subject,
+            message: message,
+        };
+
+        emailjs.send(
+            'service_2l7h6dd',
+            'template_8oc48yn',
+            templateParams,
+            'user_EXb9DCu4js07untJgFSES'
+        );
+        
+        this.resetForm();
+    }
+
+    resetForm()
+    {
+        this.setState({
+            name: '',
+            email: '',
+            subject: '',
+            message: '',
+        });
+    }
+
+    handleChange = (param: any, e: any) =>
+    {
+        this.setState({
+            [param]: e.target.value
+        });
+    }
+
     render(): JSX.Element
     {
         return (
             <Fragment>
-                <Support.SECTION>
-                    <Support.SECTION_DIV>
-                        <Support.FORM>
-                            <div id="header">
-                                <Support.HORZ_LIST>
-                                    <li>Support</li>
-                                </Support.HORZ_LIST>
-                            </div>
-                            <div>
-                                <label>Please Enter Question:</label>
-                            </div>
-                            <div>
-                                <Support.TEXTAREA id="support" name="support"></Support.TEXTAREA>
-                            </div>
-                            <div>
-                                <Support.INPUT_SUMBIT type="submit" value="Submit"/>
-                            </div>
-                        </Support.FORM>
-                    </Support.SECTION_DIV>
-                </Support.SECTION>
+                <>
+                    <h1 className="p-heading1">Get in Touch</h1>
+                    <Form onSubmit={this.handleSubmit.bind(this)}>
+                        <FormGroup controlId="formBasicEmail">
+                            <Label className="text-muted">Email address</Label>
+                            <Input
+                                type="email"
+                                name="email"
+                                value={this.state.email}
+                                className="text-primary"
+                                onChange={this.handleChange.bind(this, 'email')}
+                                placeholder="Enter email"
+                            />
+                        </FormGroup>
+                        <FormGroup controlId="formBasicName">
+                            <Label className="text-muted">Name</Label>
+                            <Input
+                                type="text"
+                                name="name"
+                                value={this.state.name}
+                                className="text-primary"
+                                onChange={this.handleChange.bind(this, 'name')}
+                                placeholder="Name"
+                            />
+                        </FormGroup>
+                        <FormGroup controlId="formBasicSubject">
+                            <Label className="text-muted">Subject</Label>
+                            <Input
+                                type="text"
+                                name="subject"
+                                className="text-primary"
+                                value={this.state.subject}
+                                onChange={this.handleChange.bind(this, 'subject')}
+                                placeholder="Subject"
+                            />
+                        </FormGroup>
+                        <FormGroup controlId="formBasicMessage">
+                            <Label className="text-muted">Message</Label>
+                            <Input
+                                type="textarea"
+                                name="message"
+                                className="text-primary"
+                                value={this.state.message}
+                                onChange={this.handleChange.bind(this, 'message')}
+                            />
+                        </FormGroup>
+                        <Button variant="primary" type="submit">
+              Submit
+                        </Button>
+                    </Form>
+                </>
             </Fragment>
         );
     }
 }
+
 export { SupportComponent };
