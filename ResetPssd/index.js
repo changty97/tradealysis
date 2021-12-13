408c9489b7c0f6ba0bf6652306e5db53e66bd709
Jonathan Huerta <jhuerta@DESKTOP-Q5E45LN.localdomain>
2021-12-13 14:10:11 -0800

    js file to change password

---
+++ ResetPssd/index.js
@@ -0,0 +1,18 @@
+// npm init 
+// npm install crypto-js
+// node index.js
+
+var CryptoJS = require("crypto-js");
+
+function getSHA3(text)
+{
+	if(text) {
+		return CryptoJS.SHA3(text, {
+			outputLength: 256
+		 }).toString();
+	}
+	return "";
+}
+let myNewPassword = "newPassword"; /** Whatever your password you want to change to **/
+console.log("We are hashing password '" + myNewPassword + "'");
+console.log(getSHA3(myNewPassword)); /** What should be stored in user_db.userTable collection (column pssd) **/
